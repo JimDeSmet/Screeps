a50e601a5f4d59484045323bd231e8b01b288610
Jimmy De Smet <J773@telenet.be>
2017-01-10 02:10:29 +0100

    trying to fix upgrader

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,20 +2,23 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
+        if(creep.carry.energy < creep.carryCapacity || creep.memory.IsHarvesting) {
+            
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
+            creep.IsHarvesting = false;
+            
         }
         else {
-            while(creep.carry.energy > 0){
                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.controller);
-                }
-            }
-        }
+                     }
+                creep.IsHarvesting = true;
+             }
     }
 };
 
 module.exports = roleUpgrader;
+
--- spawn.upgrader.js
+++ spawn.upgrader.js
@@ -1,5 +1,6 @@
 global.spawnupgrader = function() {
 
         var newUpgraderName = Game.spawns['Base'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
+        Game.creeps[newUpgraderName].memory.IsHarvesting = true;
         console.log('Spawning new upgrader: ' + newUpgraderName);
 }
