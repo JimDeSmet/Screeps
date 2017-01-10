0afab4506ebd220d436fe38ed6bb45ebebf307b7
Jimmy De Smet <J773@telenet.be>
2017-01-10 02:15:01 +0100

    hh

--- spawn.upgrader.js
+++ spawn.upgrader.js
@@ -1,6 +1,5 @@
 global.spawnupgrader = function() {
 
-        var newUpgraderName = Game.spawns['Base'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
-        Game.creeps[newUpgraderName].memory.IsHarvesting = true;
+        var newUpgraderName = Game.spawns['Base'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader', IsHarvesting : true});
         console.log('Spawning new upgrader: ' + newUpgraderName);
 }
