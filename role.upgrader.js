c8b8a8f6ee566d60d286329638095a4972fc3b88
Jimmy De Smet <J773@telenet.be>
2017-01-10 02:57:42 +0100

    dd

--- role.upgrader.js
+++ role.upgrader.js
@@ -4,23 +4,23 @@ var roleUpgrader = {
     run: function(creep) {
 
         if(creep.memory.harvesting && creep.carry.energy == 0) {
-            creep.memory.harvesting = false;
+            creep.memory.harvesting = true;
             creep.say('harvesting');
         }
         if(!creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.harvesting = true;
+            creep.memory.harvesting = false;
             creep.say('upgrading');
         }
 
 
-        if(!creep.memory.building){
+        if(creep.memory.building){
             
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }            
         }
-        else if(creep.memory.building) {
+        else {
                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.controller);
                      }
