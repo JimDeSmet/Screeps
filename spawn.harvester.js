a6ee87d258abf8ba795a03d68e1bfadd2ac77ff3
Jimmy De Smet <J773@telenet.be>
2017-01-10 01:04:16 +0100

    changed harvester spwn script

--- spawn.harvester.js
+++ spawn.harvester.js
@@ -1,5 +1,5 @@
 global.spawnharvester = function() {
 
-     var newHarvesterName = Game.spawns['Base'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
+     var newHarvesterName = Game.spawns['Base'].createCreep([WORK, CARRY, MOVE], undefined, {role : 'harvester'});
      console.log('Spawning new harvester: ' + newHarvesterName);
 }
