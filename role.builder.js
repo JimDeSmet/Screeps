96197b0241d3a96d3cad882dbe37b9253ea004f2
Jimmy De Smet <J773@telenet.be>
2017-01-10 02:59:53 +0100

    builders

--- role.builder.js
+++ role.builder.js
@@ -4,15 +4,15 @@ var roleBuilder = {
     run: function(creep) {
 
         if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
+            creep.memory.building = true;
             creep.say('harvesting');
         }
         if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
+            creep.memory.building = false;
             creep.say('building');
         }
 
-        if(!creep.memory.building) {
+        if(creep.memory.building) {
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
